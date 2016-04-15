7d5d35f68f0960e5fd2bdb1f2c218c3f0b338e24
kingsmiler <kingsmiler@163.com>
2016-04-15 17:24:26 +0800

    add undertown sample

---
+++ redis-restful/README.md
@@ -0,0 +1,4 @@
+# redis-restful
+
+keytool -genkey -alias undertown -keyalg RSA -keystore keystore.jks -validity 360
+https://www.javacodegeeks.com/2014/07/java-keystore-tutorial.html
---
+++ redis-restful/src/test/java/org/xman/nosql/undertown/HelloWorldHttpsServer.java
@@ -0,0 +1,53 @@
+package org.xman.nosql.undertown;
+
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.KeyStore;
+
+public class HelloWorldHttpsServer {
+
+    public static void main(String[] args) throws Exception {
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(getKeyManagers(), null, null);
+
+        Undertow.builder().addHttpsListener(8443, "0.0.0.0", sslContext)
+                .setHandler(new HttpHandler() {
+                    @Override
+                    public void handleRequest(final HttpServerExchange exchange) throws Exception {
+                        exchange.getResponseHeaders().put(
+                                Headers.CONTENT_TYPE,
+                                "text/plain");
+
+                        exchange.getResponseSender().send("Hello World");
+                    }
+                }).build().start();
+    }
+
+    private static KeyManager[] getKeyManagers() {
+        String pass = "lib.pass";
+        try {
+            KeyStore keyStore = KeyStore.getInstance("JKS");
+
+            keyStore.load(new FileInputStream("keystore.jks"), pass.toCharArray());
+
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory
+                    .getInstance(KeyManagerFactory.getDefaultAlgorithm());
+
+            keyManagerFactory.init(keyStore, pass.toCharArray());
+
+            return keyManagerFactory.getKeyManagers();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
